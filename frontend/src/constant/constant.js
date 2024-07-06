557fdda5225e10f10f177fcbb2f62a5fdd4eb293
Shivani Kolanu <48211558+ShivaniKolanu@users.noreply.github.com>
2024-07-06 19:31:16 -0400

    Create constant.js

---
+++ frontend/src/constant/constant.js
@@ -0,0 +1,93 @@
+const contractAddress = "put your contract address";
+
+const contractAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "int256[]",
+          "name": "_candidateIds",
+          "type": "int256[]"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "int256",
+          "name": "_id",
+          "type": "int256"
+        }
+      ],
+      "name": "addCandidate",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "candidates",
+      "outputs": [
+        {
+          "internalType": "int256",
+          "name": "id",
+          "type": "int256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "voteCount",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAllVotesofCandidates",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "int256",
+              "name": "id",
+              "type": "int256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "voteCount",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Voting.Candidate[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "vote",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+
+export {contractAbi, contractAddress};
